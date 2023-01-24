d6a586200261b94c4f3e0e2ea9bb890c448d0f64
Zamoa0714 <samuelzhao0714@gmail.com>
2023-01-24 17:10:31 -0600

    Constants class

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,7 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+  public static class DriveTrainConstants{
+    public static final int kDrivetrainLeftFrontCANID = 0;
+  }
 }
